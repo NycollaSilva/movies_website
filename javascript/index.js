ccb95433dac435c26abfdecd8e4f46c97eb244c2
Nycollas Silva <contato.nsilvaa@gmail.com>
2025-01-17 12:13:37 -0300

    Add horizontal scrolling to mouse-controlled index

--- javascript/index.js
+++ javascript/index.js
@@ -137,3 +137,12 @@ function scrollRight(containerSelector) {
         behavior: 'smooth'
     });
 }
+
+document.querySelectorAll('.movie-grid, .series-grid').forEach(container => {
+    container.addEventListener('wheel', (event) => {
+        if (event.deltaY !== 0) {
+            event.preventDefault();
+            container.scrollLeft += event.deltaY;
+        }
+    });
+});
